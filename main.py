3b88983c743ad070270dff91709c72d38d5ceaf3
Ganvs <gustavo.anverze@gmail.com>
2025-07-28 22:28:22 -0300

    asteroid class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
 
 def main():
     pygame.init()
@@ -10,11 +11,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
